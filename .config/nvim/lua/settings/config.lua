c7f297d0921f1596913a48dab0f1944b3d6e1d4c
m0xAy <126299768+m0xAy@users.noreply.github.com>
2023-03-18 19:31:15 +1300

    change theme to tokyonight-moon

--- .config/nvim/lua/settings/config.lua
+++ .config/nvim/lua/settings/config.lua
@@ -1,3 +1,3 @@
-vim.cmd([[ colorscheme tokyonight ]])
+vim.cmd([[ colorscheme tokyonight-moon ]])
 vim.cmd([[ autocmd BufWritePre * %s/\s\+$//e ]]) -- trim trailing whitespace before write
 vim.cmd([[ autocmd FocusGained,BufEnter * :checktime ]]) -- refresh file when changing buffers
