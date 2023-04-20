6988b9ce159a14103092f1450653ead86a819207
m0xAy <126299768+m0xAy@users.noreply.github.com>
2023-04-20 13:11:17 +1200

    fix treesitter error on launch

--- .config/nvim/lua/plugins/configs/treesitter.lua
+++ .config/nvim/lua/plugins/configs/treesitter.lua
@@ -2,6 +2,7 @@ local treesitter_configs = require("nvim-treesitter.configs")
 
 treesitter_configs.setup({
   ensure_installed = { "c", "lua", "vim", "help" },
+  ignore_install = { "help" },
   auto_install = true,
   highlight = {
     enable = true,
