2dae64d9292d85c607ee53071d18d24051cfd5f6
m0xAy <126299768+m0xAy@users.noreply.github.com>
2023-04-28 20:13:50 +1200

    add format on save

--- .config/nvim/lua/settings/config.lua
+++ .config/nvim/lua/settings/config.lua
@@ -1,3 +1,4 @@
 vim.cmd([[ colorscheme tokyonight-moon ]])
 vim.cmd([[ autocmd BufWritePre * %s/\s\+$//e ]]) -- trim trailing whitespace before write
 vim.cmd([[ autocmd FocusGained,BufEnter * :checktime ]]) -- refresh file when changing buffers
+vim.cmd([[ autocmd BufWritePre * lua vim.lsp.buf.format() ]]) -- format on save
