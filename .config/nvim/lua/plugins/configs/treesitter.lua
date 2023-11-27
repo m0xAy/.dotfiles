046935a34710e0a61ea3370a0a78a9b4ea5d9bf2
m0xAy <126299768+m0xAy@users.noreply.github.com>
2023-11-28 07:48:28 +1300

    update treesitter config

--- .config/nvim/lua/plugins/configs/treesitter.lua
+++ .config/nvim/lua/plugins/configs/treesitter.lua
@@ -1,4 +1,5 @@
 local treesitter_configs = require("nvim-treesitter.configs")
+local ts_context_commentstring = require("ts_context_commentstring")
 
 treesitter_configs.setup({
   ensure_installed = { "c", "lua", "vim", "help" },
@@ -11,10 +12,11 @@ treesitter_configs.setup({
   indent = {
     enable = true,
   },
-  context_commentstring = {
-    enable = true,
-  },
   autotag = {
     enable = true,
   },
 })
+
+ts_context_commentstring.setup()
+
+vim.g.skip_ts_context_commentstring_module = true
